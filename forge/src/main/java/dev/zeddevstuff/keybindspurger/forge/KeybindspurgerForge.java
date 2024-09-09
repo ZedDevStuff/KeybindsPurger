314102cf4e95b39b47f8434b0a563d099d2f0860
ZedDevStuff <kouamejuniorauguste@gmail.com>
2024-09-09 22:48:18 +0000

    Removed remaining ArchitecturyAPI references

--- forge/src/main/java/dev/zeddevstuff/keybindspurger/forge/KeybindspurgerForge.java
+++ forge/src/main/java/dev/zeddevstuff/keybindspurger/forge/KeybindspurgerForge.java
@@ -1,7 +1,6 @@
 package dev.zeddevstuff.keybindspurger.forge;
 
 import dev.zeddevstuff.keybindspurger.Keybindspurger;
-import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -10,9 +9,6 @@ public final class KeybindspurgerForge
 {
     public KeybindspurgerForge()
     {
-        // Submit our event bus to let Architectury API register our content on the right time.
-        EventBuses.registerModEventBus(Keybindspurger.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-
         // Run our common setup.
         Keybindspurger.init();
     }
